3d74d53db728359502636a55637fcae20729f110
Luis Herranz Mañas <luishmanas@gmail.com>
2018-09-14 15:30:28 +0200

    Quick update

--- deepstorm.js
+++ deepstorm.js
@@ -3,14 +3,19 @@
 const fs = require('fs');
 const ls = require('os');
 const init = require("./ds_modules/controllers/ds_init")
+const Parser = require('./hots-parser/parser.js')
 
 console.log("Testeando... %s", )
 
-
+/*  Test    */
 var inst = new init.Instance();
 inst.name = "luis";
 inst.sayHello();
 inst.CheckHostReplayParser();
+
+
+/* parse a replay*/
+Parser.processReplay(hots_replay_file, options ={});
 
 
 
