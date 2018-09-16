66b11e3e35ab82c5e72b0a3bbd6c76b383f97358
Luis Herranz Mañas <luishmanas@gmail.com>
2018-09-16 20:45:25 +0200

    Vars in json

--- deepstorm.js
+++ deepstorm.js
@@ -2,10 +2,27 @@
 
 const fs = require('fs');
 const ls = require('os');
-const init = require("./ds_modules/controllers/ds_init")
-const Parser = require('./hots-parser/parser.js')
+const init = require("./ds_modules/controllers/ds_init");
+const Parser = require('./hots-parser/parser.js');
 
-console.log("Testeando... %s", )
+/*  VAR FILES */
+//var json_replay_inforef = require("./environment/replay_inforef.json");
+
+const replay_sample_path = "./samples/";
+const replay_sample_1 = "CH_gamemode_leagueT_leagueB_000000.StormReplay";
+
+
+
+/* This on a different VAR file */ 
+const replay_format_postfix=".StormReplay";
+
+replay_sample_path.concat(replay_sample_1);
+
+
+/*  INIT     */
+//console.log(replay_sample_path );
+
+console.log("Testeando... %s", );
 
 /*  Test    */
 var inst = new init.Instance();
@@ -14,8 +31,10 @@ inst.sayHello();
 inst.CheckHostReplayParser();
 
 
+hots_replay_file = replay_sample_path;
 /* parse a replay*/
-Parser.processReplay(hots_replay_file, options ={});
+// if( (fs.existsSync(hots_replay_file))
+//     Parser.processReplay(hots_replay_file);
 
 
 
